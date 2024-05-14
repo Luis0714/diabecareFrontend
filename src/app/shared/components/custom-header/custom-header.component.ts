113b0aa42c8ca0f17e4ce59b8be51e08b8bbe1b9
julianagomez21 <jgomezgal25@gmail.com>
2024-05-13 23:54:55 -0500

    Se crean las propiedades width y height para el logo y se utiliza dentro del componente del encabazado. Además, se agrega título y botón de notificación al encabezado.

--- src/app/shared/components/custom-header/custom-header.component.ts
+++ src/app/shared/components/custom-header/custom-header.component.ts
@@ -1,14 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton } from '@ionic/angular/standalone';
+import { IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton, IonIcon } from '@ionic/angular/standalone';
+import { CustomLogoComponent } from '../custom-logo/custom-logo.component';
 
 @Component({
   selector: 'app-custom-header',
   templateUrl: './custom-header.component.html',
   styleUrls: ['./custom-header.component.scss'],
   standalone: true,
-  imports: [
+  imports: [IonIcon, 
     RouterLink,
     IonBackButton, IonButtons,
     IonHeader,
@@ -16,6 +17,7 @@ import { IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton } from '@ion
     IonToolbar,
     CommonModule,
     IonBackButton,
+    CustomLogoComponent
 
   ]
 })
