34487bda8182dc6aef534960fd8efd42c8db20d9
Yair Sanchez <yair.sanchez21369@ucaldas.edu.co>
2024-05-29 22:52:54 -0500

    primera maqueta view-plan item F

--- src/app/views/domain/pages/planes/view-plan/view-plan.component.ts
+++ src/app/views/domain/pages/planes/view-plan/view-plan.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent } from "@ionic/angular/standalone";
+import { IonCard } from "@ionic/angular/standalone";
+import { CustomButtonComponent } from 'src/app/shared/components/custom-button/custom-button.component';
+
 
 @Component({
   selector: 'app-view-plan',
   templateUrl: './view-plan.component.html',
   styleUrls: ['./view-plan.component.scss'],
+  standalone: true,
+  imports: [IonCardContent, IonCardSubtitle, IonCardTitle, IonCardHeader, IonCard,CustomButtonComponent ]
 })
-export class ViewPlanComponent  implements OnInit {
+export class ViewPlanComponent implements OnInit {
+  recommendetions: string[];
 
-  constructor() { }
+  constructor() {
+    this.recommendetions = ['Tomar agua', 'Hacer ejercicio', 'Comer frutas'];
+  }
 
   ngOnInit() {}
-
 }
